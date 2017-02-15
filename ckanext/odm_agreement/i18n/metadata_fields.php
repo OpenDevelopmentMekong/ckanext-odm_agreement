1bec4d8ce10f7083bc7d530b8038f3f23ad9927e
Alex Corbi <a.corbi@gmail.com>
2017-02-15 15:30:31 +0100

    finished schema

--- ckanext/odm_agreement/i18n/metadata_fields.php
+++ ckanext/odm_agreement/i18n/metadata_fields.php
@@ -1,5 +1,24 @@
 <?php
 // This template allows translation tools such as POEDIT to detect automatically the metadata labels to translate
-echo _('Agreement');
+echo _('Contract Name');
+echo _('Objective');
+echo _('Participating share');
+echo _('Contracting Parties');
+echo _('Concession/License name');
+echo _('Disclosure Mode');
+echo _('LandMatrix No.');
+echo _('Document reference no.');
+echo _('Amendment to contract');
+echo _('Short notes of change');
+echo _('Signature date');
+echo _('Related documents');
+echo _('Links to source');
+echo _('Granted area');
+echo _('Contract term');
+echo _('Payment for concession fees');
+echo _('Guaranty deposit for contract implementation');
+echo _('');
+echo _('');
+echo _('');
 
 ?>
