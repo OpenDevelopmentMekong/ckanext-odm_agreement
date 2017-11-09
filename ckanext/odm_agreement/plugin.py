2b2465c9cb2e538d9e35b3fd7a573cf0ecfd4fb7
Alex Corbi <a.corbi@gmail.com>
2017-11-09 12:56:23 +0100

    enabling review_system by default

--- ckanext/odm_agreement/plugin.py
+++ ckanext/odm_agreement/plugin.py
@@ -79,8 +79,8 @@ class OdmAgreementPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     if dataset_type == 'agreement':
       log.debug('after_create: %s', pkg_dict['name'])
 
-      # Create default Issue
-      review_system = config.get("ckanext.issues.review_system", False) == "true"
+      #review_system = config.get("ckanext.issues.review_system", False) == "true"
+      review_system = True
       if review_system:
         if pkg_dict['type'] == 'agreement':
           odm_agreement_helper.create_default_issue_agreement(pkg_dict)
