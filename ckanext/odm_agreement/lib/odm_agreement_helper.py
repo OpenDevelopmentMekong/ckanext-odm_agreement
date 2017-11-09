2e03b4b959d454c4fe999040867621092d45f5c8
Alex Corbi <a.corbi@gmail.com>
2017-11-09 11:40:27 +0100

    using toolkit.asbool, removing loader_class

--- ckanext/odm_agreement/lib/odm_agreement_helper.py
+++ ckanext/odm_agreement/lib/odm_agreement_helper.py
@@ -8,7 +8,6 @@ import os
 import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from genshi.template.text import NewTextTemplate
 from ckan.lib.base import render
 
 DEBUG = True
@@ -30,7 +29,7 @@ def create_default_issue_agreement(pkg_info):
 			't0': toolkit._("Thank you for uploading this item. Instructions about vetting system available on https://wiki.opendevelopmentmekong.net/partners:content_review#instructions_for_default_issue_on_agreement")
 		}
 
-		issue_message = render('messages/default_issue_agreement.txt',extra_vars=extra_vars,loader_class=NewTextTemplate)
+		issue_message = render('messages/default_issue_agreement.txt',extra_vars=extra_vars)
 
 		params = {'title':'User Agreement Upload Checklist','description':issue_message,'dataset_id':pkg_info['id']}
 		toolkit.get_action('issue_create')(data_dict=params)
