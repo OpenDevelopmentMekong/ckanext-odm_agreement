20a854750d18883149fde4c26fe6525e3aff2c0e
Alex Corbi <a.corbi@gmail.com>
2017-12-05 10:41:37 +0100

    updated i18n

---
+++ ckanext/odm_agreement/i18n/odm_agreement_document_type.php
@@ -0,0 +1,22 @@
+<?php
+// This template allows translation tools such as POEDIT to detect automatically the metadata labels to translate
+echo _("Contract");
+echo _("Contract Amendment");
+echo _("Contract Annex");
+echo _("Contract Farming Agreement");
+echo _("Energy Contract");
+echo _("Exploration Permit/License");
+echo _("Exploitation/extraction permit or license");
+echo _("Forest Concession Contract");
+echo _("In-land Fishcatchment Concession Contract");
+echo _("Infrastructure Contract");
+echo _("Investment Promotion Agreement");
+echo _("Joint Venture Agreement");
+echo _("Land Concession Contract");
+echo _("Memorandum of Understanding");
+echo _("Maritime Fishcatchment Concession Contract");
+echo _("Other long-term leasing contract of State Assets ");
+echo _("Production or Profit Sharing Agreement");
+echo _("Service Contract");
+echo _("Social Land Concession");
+?>
