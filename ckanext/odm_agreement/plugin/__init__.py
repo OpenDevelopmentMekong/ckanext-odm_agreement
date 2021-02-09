d971a5658173d5750c185294423fae4c3d1e8983
gtkChop <swaroopbhatk@gmail.com>
2021-02-09 13:34:07 +0000

    templates and routes for ckan 2.9.1

--- ckanext/odm_agreement/plugin/__init__.py
+++ ckanext/odm_agreement/plugin/__init__.py
@@ -16,59 +16,51 @@ import ckan.lib.helpers as h
 log = logging.getLogger(__name__)
 
 
-try:
-    toolkit.requires_ckan_version("2.9")
-except CkanVersionException:
-    from ckanext.odm_agreement.plugin.pylons_plugin import OdmAgreementMixinPlugin
-else:
+if toolkit.check_ckan_version(min_version='2.9.0'):
     from ckanext.odm_agreement.plugin.flask_plugin import OdmAgreementMixinPlugin
+else:
+    from ckanext.odm_agreement.plugin.pylons_plugin import OdmAgreementMixinPlugin
 
 
 class OdmAgreementPlugin(OdmAgreementMixinPlugin):
-  '''OD Mekong agreement plugin.'''
+    '''OD Mekong agreement plugin.'''
 
-  plugins.implements(plugins.IValidators, inherit=True)
-  plugins.implements(plugins.IConfigurer)
-  plugins.implements(plugins.ITemplateHelpers)
-  plugins.implements(plugins.IPackageController, inherit=True)
+    plugins.implements(plugins.IValidators, inherit=True)
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
-  def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
 
-    log.debug('OdmAgreementPlugin init')
-    wsgi_app = SessionMiddleware(None, None)
-    odm_agreement_helper.session = wsgi_app.session
+        log.debug('OdmAgreementPlugin init')
+        wsgi_app = SessionMiddleware(None, None)
+        odm_agreement_helper.session = wsgi_app.session
 
-  def update_config(self, config):
-    '''Update plugin config'''
+    def get_helpers(self):
+        '''Register the plugin's functions above as a template helper function.'''
 
-    toolkit.add_template_directory(config, 'templates')
-    toolkit.add_public_directory(config, 'public')
+        return {
+          'odm_agreement_get_dataset_type': odm_agreement_helper.get_dataset_type,
+          'odm_agreement_validate_fields' : odm_agreement_helper.validate_fields
+        }
 
-  def get_helpers(self):
-    '''Register the plugin's functions above as a template helper function.'''
+    def get_validators(self):
+        '''Register the plugin's functions above as validators.'''
 
-    return {
-      'odm_agreement_get_dataset_type': odm_agreement_helper.get_dataset_type,
-      'odm_agreement_validate_fields' : odm_agreement_helper.validate_fields
-    }
+        log.debug("get_validators")
 
-  def get_validators(self):
-    '''Register the plugin's functions above as validators.'''
+        return {
+          'odm_agreement_generate_ocds_id': odm_agreement_helper.generate_ocds_id
+        }
 
-    log.debug("get_validators")
+    def after_create(self, context, pkg_dict):
+        dataset_type = context['package'].type if 'package' in context else pkg_dict['type']
 
-    return {
-      'odm_agreement_generate_ocds_id': odm_agreement_helper.generate_ocds_id
-    }
+        if dataset_type == 'agreement':
+            log.debug('after_create: %s', pkg_dict['name'])
 
-  def after_create(self, context, pkg_dict):
-    dataset_type = context['package'].type if 'package' in context else pkg_dict['type']
-
-    if dataset_type == 'agreement':
-      log.debug('after_create: %s', pkg_dict['name'])
-
-      review_system = toolkit.asbool(config.get("ckanext.issues.review_system", False))
-      if review_system:
-        if pkg_dict['type'] == 'agreement':
-          odm_agreement_helper.create_default_issue_agreement(pkg_dict)
+            review_system = toolkit.asbool(config.get("ckanext.issues.review_system", False))
+            if review_system:
+                if pkg_dict['type'] == 'agreement':
+                    odm_agreement_helper.create_default_issue_agreement(pkg_dict)
 
--- ckanext/odm_agreement/plugin/pylons_plugin.py
+++ ckanext/odm_agreement/plugin/pylons_plugin.py
@@ -5,22 +5,28 @@ log = logging.getLogger(__name__)
 
 
 class OdmAgreementMixinPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
-  '''OD Mekong agreement plugin pylons.'''
-  plugins.implements(plugins.IRoutes, inherit=True)
+    '''OD Mekong agreement plugin pylons.'''
+    plugins.implements(plugins.IRoutes, inherit=True)
 
-  def before_map(self, m):
+    def update_config(self, config):
+        '''Update plugin config'''
 
-    m.connect('odm_agreement_index','/agreement',controller='package',type='agreement',action='search')
+        toolkit.add_template_directory(config, '../templates')
+        toolkit.add_public_directory(config, '../public')
 
-    m.connect('odm_agreement_new','/agreement/new',controller='package',type='agreement',action='new')
+    def before_map(self, m):
 
-    m.connect('odm_agreement_new_resource','/agreement/new_resource/{id}',controller='package',type='agreement',action='new_resource')
+        m.connect('odm_agreement_index','/agreement',controller='package',type='agreement',action='search')
 
-    m.connect('odm_agreement_read', '/agreement/{id}',controller='package',type='agreement', action='read', ckan_icon='book')
+        m.connect('odm_agreement_new','/agreement/new',controller='package',type='agreement',action='new')
 
-    m.connect('odm_agreement_edit', '/agreement/edit/{id}',controller='package',type='agreement', action='edit')
+        m.connect('odm_agreement_new_resource','/agreement/new_resource/{id}',controller='package',type='agreement',action='new_resource')
 
-    m.connect('odm_agreement_delete', '/agreement/delete/{id}',controller='package',type='agreement', action='delete')
+        m.connect('odm_agreement_read', '/agreement/{id}',controller='package',type='agreement', action='read', ckan_icon='book')
 
-    return m
+        m.connect('odm_agreement_edit', '/agreement/edit/{id}',controller='package',type='agreement', action='edit')
+
+        m.connect('odm_agreement_delete', '/agreement/delete/{id}',controller='package',type='agreement', action='delete')
+
+        return m
 
